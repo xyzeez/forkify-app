019403aaf445ca8099e98f61048b974c133a0577
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-02-20 19:03:55 +0300

    ✨ feat: start app functionalities

--- src/js/config.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-
-export const API_KEY = '0d48a86e-9ac2-4f72-8962-6f510189654a';
-
-export const TIMEOUT_SEC = 5;
-
-export const RES_PER_PAGE = 10;
-
-export const MODAL_CLOSE_SEC = 3;

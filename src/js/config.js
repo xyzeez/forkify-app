2b9b0a92a2723a618a36dca39ac146ee5032029e
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-02-27 15:42:49 +0300

    🔧 chore: update API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
-export const API_KEY = '3ce683eb-ebd1-4c8d-88e0-4355d180febf';
+export const API_KEY = '076e08e2-df74-4bfb-9dca-3e2d6f8d14b4';
 
 export const FETCH_TIMEOUT_SEC = 10;
 
